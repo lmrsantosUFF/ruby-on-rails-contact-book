e86df2e5166879c3c01546b63adbbd65e94e4955
Leonardo Matsumoto <lmrsantos@gmail.com>
2022-04-13 00:25:39 -0400

    hello world

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+  def index
+    @nome = "Matsu"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+root to: "static_pages#index"
+
 end
